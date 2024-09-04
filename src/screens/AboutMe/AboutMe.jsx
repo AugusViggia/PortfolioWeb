a10cd665b198a5e62af8259c95dd61856a4e9169
Augusto Viggiano <112136936+AugusViggia@users.noreply.github.com>
2024-09-04 18:39:02 -0300

    modificado about me

--- src/screens/AboutMe/AboutMe.jsx
+++ src/screens/AboutMe/AboutMe.jsx
@@ -11,13 +11,23 @@ function AboutMe() {
         <div className="about__text">
           <h1>ABOUT ME</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quia
-            placeat eos atque veniam voluptatum sapiente ipsum tempore iste,
-            omnis cumque velit autem consectetur. Veritatis molestias ullam
-            voluptas nostrum? Dolorum. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Eum ipsam iusto suscipit tempore incidunt,
-            sapiente consequatur nihil repellendus quasi beatae explicabo
-            eveniet, voluptatem vitae quos recusandae minus, quia iure odio.
+            I am a passionate Full Stack Web Developer with a strong foundation
+            in both frontend and backend technologies. Currently pursuing a
+            Bachelor's degree in Systems Engineering at UADE, I am committed to
+            continuous learning and personal development. My goal is to stay at
+            the forefront of technology, constantly acquiring new skills and
+            applying them to real-world projects. I thrive on challenges and
+            enjoy creating innovative solutions that not only meet client needs
+            but also contribute to my growth as a developer. Whether working
+            independently or as part of a team, I am always driven by a desire
+            to deliver high-quality, scalable, and efficient code.
+          </p>
+          <p>
+            In addition to my technical expertise, I am dedicated to building a
+            strong professional network and collaborating with others in the
+            tech community. I am always open to new opportunities that allow me
+            to contribute my skills and knowledge, while also learning from
+            others. Let's connect and create something amazing together!
           </p>
         </div>
       </div>
--- src/screens/ContactMe/ContactMe.jsx
+++ src/screens/ContactMe/ContactMe.jsx
@@ -1,29 +1,29 @@
 import React from 'react'
 import './ContactMe.css'
-import { navigationLinks } from '../../helpers/navLinks';
+// import { navigationLinks } from '../../helpers/navLinks';
 
-function createLinks() {
-  return navigationLinks.map((link, index) => {
-    return (
-      <a key={index} href={link.url} target="_blank" rel="noreferrer">
-        {link.name}
-      </a>
-    )
-  })
-} 
+// function createLinks() {
+//   return navigationLinks.map((link, index) => {
+//     return (
+//       <a key={index} href={link.url} target="_blank" rel="noreferrer">
+//         {link.name}
+//       </a>
+//     )
+//   })
+// } 
 
 function ContactMe() {
   return (
     <div id="contact" className="contactMe_container">
       <div className="contactMe_data_links">
-        <div>
-          <p>Buenos Aires, ARG</p>
-          <p>Augusto Viggiano</p>
-          <p>augusto.viggiano@gmail.com</p>
-        </div>
-        <div>{createLinks()}</div>
+        <p>Buenos Aires, ARG</p>
+        <p>Augusto Viggiano</p>
+        <p>augusto.viggiano@gmail.com</p>
       </div>
-      <div>Copyright&copy; {new Date().getFullYear()} All rights reserved</div>
+      {/* <div>{createLinks()}</div> */}
+      <div className="copyright">
+        Copyright&copy; {new Date().getFullYear()} All rights reserved
+      </div>
     </div>
   );
 }
